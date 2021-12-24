08f9add9d18a9d72aad1dfd8ebf041da9de67492
unknown <ldy9212@gmail.com>
2021-12-24 20:32:12 +0900

    2112

---
+++ pages/_document.tsx
@@ -0,0 +1,52 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&family=Roboto:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <meta name="description" content="DY Template" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ styles/GlobalStyle.tsx
@@ -0,0 +1,27 @@
+import reset from 'styled-reset';
+import { createGlobalStyle, css } from 'styled-components';
+
+const globalStyle = css`
+  ${reset};
+  @font-face {
+    font-family: 'Noto Sans KR', sans-serif;
+    unicode-range: U+AC00-D7A3;
+  }
+  @font-face {
+    font-family: 'Roboto', sans-selif;
+    unicode-range: U+0041-005A, U+0061-007A, U+0020-002F, U+003A-0040, U+005B-0060, U+007B-007E;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0px;
+    padding: 0px;
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+${globalStyle}
+`;
+
+export default GlobalStyle;
